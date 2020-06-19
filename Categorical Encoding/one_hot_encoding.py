adaf76c5af3887b4d39ca6d1ea5132c6e0692917
ADARSH R <adarshraj1003@gmail.com>
2020-06-19 13:00:00 +0530

    added one hot encoding

---
+++ Categorical Encoding/one_hot_encoding.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+from Data_Preparation import *
+print(obj_df['drive_wheels'])
+
+#convert each category values into a new column and assign 1 and 0
+
+#the new dataset contains three columns drive_wheels_4wd, drive_wheels_fwd and drive_wheels_rwd
+
+pd.get_dummies(obj_df, columns = ['body_style','drive_wheels'], prefix = ['body', 'drive']).head()
