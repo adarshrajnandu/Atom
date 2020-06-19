b85119ab23735775aeae8f74a11f1bd1acdd9775
Adarsh R <adarshraj1003@gmail.com>
2020-06-19 13:13:25 +0530

    use select_dtypes after one hot encoding

--- Categorical Encoding/one_hot_encoding.py
+++ Categorical Encoding/one_hot_encoding.py
@@ -5,6 +5,11 @@ print(obj_df['drive_wheels'])
 
 #convert each category values into a new column and assign 1 and 0
 
-#the new dataset contains three columns drive_wheels_4wd, drive_wheels_fwd and drive_wheels_rwd
+#the new dataset contains three columns drive_wheels_4wd, drive_wheels_fwd and
+#drive_wheels_rwd
 
-pd.get_dummies(obj_df, columns = ['body_style','drive_wheels'], prefix = ['body', 'drive']).head()
+pd.get_dummies(obj_df, columns = ['body_style','drive_wheels'],
+prefix = ['body','drive']).head()
+
+#get_dummies returns the full dataframe so we need to filter out the objects
+#using select_dtypes
